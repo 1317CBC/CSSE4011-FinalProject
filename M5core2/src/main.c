8c12f8741988efe94e6d9f70001a66a5e5e5edb9
1317CBC <frc6949@gmail.com>
2024-04-29 22:50:52 +1000

    update the code for IMU

--- M5core2/src/main.c
+++ M5core2/src/main.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <zephyr/kernel.h>
 #include <math.h>
+#include <zephyr/types.h>
 
 #define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
 #include <zephyr/logging/log.h>
@@ -16,13 +17,20 @@ LOG_MODULE_REGISTER(app);
 #define RADIUS 50
 #define ANGLE_STEP 5
 
+#define NUM_SAMPLES 10
+#define SAMPLE_PERIOD_MS 100
+#define RAD_TO_DEG 57.2957795131
+
+const double ACCEL_THRESHOLD = 1;  // accelerometer threshold
+const double GYRO_THRESHOLD = 50.0;  // gyroscope threshold
+
 static lv_obj_t *label;
 static lv_obj_t *roll_lable;
 static lv_style_t label_style;
 
 typedef struct {
-    float ax, ay, az; // calibration data for accelerometer
-    float gx, gy, gz; // calibration data for gyroscope
+    double ax, ay, az; // calibration data for accelerometer
+    double gx, gy, gz; // calibration data for gyroscope
 } SensorData;
 
 
@@ -49,31 +57,70 @@ void calculateAngles(const SensorData* data, float* pitch, float* roll) {
 
 
 static void accinfo(void){
-    SensorData data;
+    SensorData data = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
     struct sensor_value accel[3], gyro[3];
     float pitch = 0.0f, roll = 0.0f;
-    char pitch_str[30];
-    char roll_str[30];
+    char pitch_str[40];
+    char roll_str[40];
 
-    sensor_sample_fetch(dev);
-    sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, accel);
-    sensor_channel_get(dev, SENSOR_CHAN_GYRO_XYZ, gyro);
+    // sensor_sample_fetch(dev);
+    // sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, accel);
+    // sensor_channel_get(dev, SENSOR_CHAN_GYRO_XYZ, gyro);
 
-    data.ax = sensor_value_to_double(&accel[0]);
-    data.ay = sensor_value_to_double(&accel[1]);
-    data.az = sensor_value_to_double(&accel[2]);
-    data.gx = sensor_value_to_double(&gyro[0]);
-    data.gy = sensor_value_to_double(&gyro[1]);
-    data.gz = sensor_value_to_double(&gyro[2]);
+    // data.ax = sensor_value_to_double(&accel[0]);
+    // data.ay = sensor_value_to_double(&accel[1]);
+    // data.az = sensor_value_to_double(&accel[2]);
+    // data.gx = sensor_value_to_double(&gyro[0]);
+    // data.gy = sensor_value_to_double(&gyro[1]);
+    // data.gz = sensor_value_to_double(&gyro[2]);
 
-    calculateAngles(&data, &pitch, &roll);
-    sprintf(pitch_str, "Pitch: %lf \n", (double)pitch);
-    sprintf(roll_str, "Roll: %lf \n", (double)roll);
+    for (int i = 0; i < NUM_SAMPLES; i++) {
+        sensor_sample_fetch(dev);
+        sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, accel);
+        sensor_channel_get(dev, SENSOR_CHAN_GYRO_XYZ, gyro);
+
+        data.ax += (sensor_value_to_double(&accel[0]) + 0.05);
+        data.ay += (sensor_value_to_double(&accel[1]) - 0.18);
+        data.az += (sensor_value_to_double(&accel[2]) - 10.45);
+        data.gx += sensor_value_to_double(&gyro[0]);
+        data.gy += sensor_value_to_double(&gyro[1]);
+        data.gz += sensor_value_to_double(&gyro[2]);
+
+        k_sleep(K_MSEC(10)); // Wait a bit between samples
+    }
+
+    data.ax = round((data.ax / NUM_SAMPLES) * 100) / 100;
+    data.ay = round((data.ay / NUM_SAMPLES) * 100) / 100;
+    data.az = round((data.az / NUM_SAMPLES) * 100) / 100;
+    data.gx = round((data.gx / NUM_SAMPLES) * 100) / 100;
+    data.gy = round((data.gy / NUM_SAMPLES) * 100) / 100;
+    data.gz = round((data.gz / NUM_SAMPLES) * 100) / 100;
+
+    double angle = atan2(data.ay, data.ax) * RAD_TO_DEG;
+
+
+    if (data.ax > ACCEL_THRESHOLD || data.ay > ACCEL_THRESHOLD ||
+        data.ax < -ACCEL_THRESHOLD || data.ay < -ACCEL_THRESHOLD) {
+        // double angle = atan2(data.ay, data.ax) * RAD_TO_DEG;
+        if ((angle > 67.5 && angle <= 112.5)) {
+            sprintf(pitch_str, "back   %lf\n", (double)angle);
+        } else if ((angle > 157.5 && angle <= 180) || (angle > -180 && angle <= -157.5)) {
+            sprintf(pitch_str, "right   %lf\n", (double)angle);
+        } else if ((angle < -67.5 && angle >= -112.5)) {
+            sprintf(pitch_str, "front    %lf\n", (double)angle);
+        } else if ((angle > -22.5 && angle <= 22.5) || (angle > 157.5 || angle < -157.5)) {
+            sprintf(pitch_str, "Left    %lf\n", (double)angle);
+        }
+    }
+
+    // calculateAngles(&data, &pitch, &roll);
+    // sprintf(pitch_str, "Pitch: %lf \n", (double)pitch);
+    // sprintf(roll_str, "Roll: %lf \n", (double)roll);
     lv_label_set_text(label,pitch_str);
-    lv_label_set_text(roll_lable,roll_str);
+    // lv_label_set_text(roll_lable,roll_str);
 
     lv_obj_align(label, LV_ALIGN_CENTER, -10, -50);
-    lv_obj_align(roll_lable, LV_ALIGN_CENTER, -10, 0);
+    // lv_obj_align(roll_lable, LV_ALIGN_CENTER, -10, 0);
     
 
 }
@@ -128,6 +175,6 @@ int main(void)
     while (1) {
         accinfo();
         lv_timer_handler();
-        k_sleep(K_MSEC(100));
+        // k_sleep(K_MSEC(100));
     }
 }
